8fb06bd11d3059dd78505375b8bb4b6730a000ca
Dipak Pawar <dipakpawar152000@gmail.com>
2021-04-18 12:13:48 +0530

    MedHunt Project

---
+++ app/src/main/java/com/example/firestore/CustomerSignUp.java
@@ -0,0 +1,13 @@
+package com.example.firestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_sign_up);
+    }
+}
---
+++ app/src/main/java/com/example/firestore/OwnerSignUp.java
@@ -0,0 +1,13 @@
+package com.example.firestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OwnerSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_sign_up);
+    }
+}
